7612217b07f3b9ae4811c16c4422a07d41875275
rokayaothman2003@gmail.com <rokayaothman2003@gmail.com>
2025-08-02 21:50:40 +0300

    solve 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let output = "";
+    for (let i = input.length-1; i >= 0; i-- )
+    {
+        output += input[i];
+    }
+    return output;
 };
 
+//console.log(reverseString("Egypt"));
 // Do not edit below this line
 module.exports = reverseString;
