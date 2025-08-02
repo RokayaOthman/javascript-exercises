abb721b6570f69c42de14ab6147af2f806081298
rokayaothman2003@gmail.com <rokayaothman2003@gmail.com>
2025-08-02 21:42:18 +0300

    solve 05 and 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    let newword = "";
+    for (let i = 0; i < num; i++)
+    {
+        newword += word;
+    }
+    return newword;
 };
 
+console.log(repeatString("roka",3));
 // Do not edit below this line
 module.exports = repeatString;
