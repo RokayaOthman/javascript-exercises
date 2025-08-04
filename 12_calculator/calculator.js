f06688c95390e1a340a359eec9f6750be2552488
rokayaothman2003@gmail.com <rokayaothman2003@gmail.com>
2025-08-04 22:08:42 +0300

    solve problem 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(...numbers) {
+  // numbers is array for as a rest parameter
+	return numbers.reduce((total,current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...numbers) {
+  return numbers.reduce((total, current) => total * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let result = 1
+	for (let i = 1; i <= b; i++) {
+    result *= a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number == 1 || number == 0)
+  {
+    return 1
+  }
+   return number * factorial(number - 1);
 };
 
 // Do not edit below this line
