2fc04441c6940ee161099df6422669ab6d3476fd
rokayaothman2003@gmail.com <rokayaothman2003@gmail.com>
2025-08-04 17:17:51 +0300

    sove problem 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,24 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+ if (year % 4 !== 0) {
+    return false;
+ }
+ else { // this might be a leap
+    if (year % 100 == 0 && year % 400 == 1)
+    {
+        return false;
+    }
+    return true;
+ }
 };
-
+if(leapYears(1985)) 
+{
+    
+    console.log("yeas");
+    
+}
+else 
+{
+    console.log("No");
+}
 // Do not edit below this line
 module.exports = leapYears;
