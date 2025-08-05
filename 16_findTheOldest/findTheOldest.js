6fe3331a521de20bf42980b297a0d1bff80a27fb
rokayaothman2003@gmail.com <rokayaothman2003@gmail.com>
2025-08-05 23:32:32 +0300

    solve problem 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let maxx = 0;
+    let theperson = "";
+    for(let i = 0; i < people.length; i++)
+    {
+    
+        let person = people[i];
+        let age = person.yearOfDeath - person.yearOfBirth;
+        
+        if (age > maxx)
+        {
+            maxx = age;
+           theperson = person
+        }
+    }
+    return theperson;
 };
 
 // Do not edit below this line
