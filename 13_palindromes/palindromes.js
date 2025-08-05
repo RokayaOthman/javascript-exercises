1e73cde5fe7158ac7ebcbaf09e7b753544b0b920
rokayaothman2003@gmail.com <rokayaothman2003@gmail.com>
2025-08-05 20:50:58 +0300

    solve 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let left = 0;
+    for(let right = word.length - 1; right > 0; right-- )
+    {
+        if(word[right] !== word[left])
+        {
+            return false;
+        }
+        left++;
+    }
+    return true;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
