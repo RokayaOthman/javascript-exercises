546b5e358274a23e1df4b3230d86c871e4c77769
rokayaothman2003@gmail.com <rokayaothman2003@gmail.com>
2025-08-05 21:42:05 +0300

    solve problem 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,30 @@
-const fibonacci = function() {
 
+const fibonacci = function(numArg) {
+    let num;
+    if (typeof num !== 'number')
+    {
+        num = parseInt(numArg);
+    }
+    else
+    {
+        num = numArg;
+    }
+
+    if (num === 0) return 0;
+    if (num === 1) return 1;
+    
+    let last = 0;
+    let current = 1;
+    let next = 0;
+
+    for (let i = 2; i <= num; i++) {
+        next = last + current;
+        last = current;
+        current = next;
+    }
+    return current;
 };
+//console.log(fibonacci(4));
 
 // Do not edit below this line
 module.exports = fibonacci;
